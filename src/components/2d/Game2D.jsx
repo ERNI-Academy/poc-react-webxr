b9939eb68df9e2b64ac17e196ccaa0af1d9d780f
Axel Taylor <axel@taylor.cat>
2022-09-02 11:52:12 +0200

    Add intro to the 2D simulation

--- src/components/2d/Game2D.jsx
+++ src/components/2d/Game2D.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 import shallow from "zustand/shallow";
 import { useGameStore } from "../../stores/gameStore";
 import Button from "../common/Button/Button";
@@ -8,6 +9,8 @@ import Label2D from "./Label2D";
 
 const Game2D = () => {
   const [isGameInit, gameInit] = useState(false);
+  const [startButtonReady, setStartButtonReady] = useState(false);
+
   const router = useRouter();
 
   const { points, timeLeft, startGame } = useGameStore(
@@ -34,9 +37,32 @@ const Game2D = () => {
 
   return !isGameInit ? (
     <div className="start-btn">
-      <Button animate onClick={() => initGame()}>
-        START
-      </Button>
+      <TypeAnimation
+        sequence={[
+          "[SYSTEM] ...",
+          200,
+          "[SYSTEM] Welcome to the Mole Attack Simulator, please wait a second...",
+          500,
+          "[SYSTEM] Virtual Reality Headset not detected...",
+          500,
+          "[SYSTEM] Loading the 2D simulator...",
+          800,
+          "[SYSTEM] Loading the 2D simulator... OK.",
+          800,
+          "[SYSTEM] The 2D simulator is ready! Please press the START button to start the simulation.",
+          500,
+          () => {
+            setStartButtonReady(true);
+          },
+          "[SYSTEM] The 2D simulator is ready! Please press the START button to start the simulation. WARNING: Proceed at your own risk, good luck.",
+        ]}
+        wrapper="p"
+      />
+      {startButtonReady && (
+        <Button animate onClick={() => initGame()}>
+          START
+        </Button>
+      )}
       <style jsx>{`
         .start-btn {
           display: block;
