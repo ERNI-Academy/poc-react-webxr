4c19f1397734367dac4617a2e940f69996be4018
David Carmona - Contractor <David.Carmona@contractor.qiagen.com>
2022-09-12 18:30:27 +0200

    add challenge fixed

--- src/components/2d/Mole2D.jsx
+++ src/components/2d/Mole2D.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import shallow from "zustand/shallow";
 import { useGameStore } from "../../stores/gameStore";
 import {useIdleTimer} from "react-idle-timer";
