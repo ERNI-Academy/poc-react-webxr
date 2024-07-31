5bd71657420d267516cf3b7159c7a9f8ff557c0f
david <davidwalker2235@gmail.com>
2024-07-31 13:51:50 +0200

    change DB

--- src/services/scoreService.js
+++ src/services/scoreService.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rwgytamqsknjrqaedebn.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPA_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 class ScoreService {
   constructor() {
